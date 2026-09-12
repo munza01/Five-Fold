215b36daceae2e5cc10b8087bea183d2cb93c74a
munza01 <munza020@gmail.com>
2026-09-13 08:45:51 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fivefold-v5';
+const CACHE='fivefold-v7';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./words.js','./icon-180.png','./icon-192.png','./icon-512.png'];
 
 self.addEventListener('install',e=>{
